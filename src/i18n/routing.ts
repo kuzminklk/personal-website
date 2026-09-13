8953628757ba316eb2a20ae9bcf6e436534185f3
kuzminkdk <kuzminkdk@gmail.com>
2026-09-13 23:59:25 +0300

    Update next-intl configuration

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,7 +4,7 @@ import { defineRouting } from "next-intl/routing"
 
 
 export const routing = defineRouting({
-	locales: ["en", "ru", "pl", "by"],
+	locales: ["en", "ru"/* , "pl", "by" */],
 	defaultLocale: "en",
 	pathnames: {
 		"/about": {
