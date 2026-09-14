9d03b2504adf6cf67ba02b124db2fd69102ba729
kuzminkdk <kuzminkdk@gmail.com>
2026-09-14 17:13:43 +0300

    Improve visual design for mobile

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -4,6 +4,7 @@
 
 
 import { useLocale } from "next-intl"
+import { useTranslations } from "next-intl"
 
 import { usePathname, useRouter } from "@/i18n/naviagation"
 import styles from "./LanguageSwitcher.module.css"
@@ -13,8 +14,11 @@ export function LanguageSwitcher() {
 	const locale = useLocale()
 	const router = useRouter()
 	const pathname = usePathname()
+	const translations = useTranslations("LanguageSwitcher")
 
 	const nextLocale = locale === "en" ? "ru" : "en"
+
+	const title = translations("title")
 
 	function switchLanguage() {
 		router.replace(pathname, {
@@ -23,7 +27,7 @@ export function LanguageSwitcher() {
 	}
 
 	return (
-		<button onClick={switchLanguage} className={styles.switcher} aria-label={`Switch to ${nextLocale}`}>
+		<button onClick={switchLanguage} className={styles.switcher} title={title} aria-label={title}>
 			{locale === "en" ? "🇺🇸" : "🇷🇺"}
 		</button>
 	)
