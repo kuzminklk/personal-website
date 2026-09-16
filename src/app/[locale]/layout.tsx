1a7dac0b417f2661f88b86d5b32fe3a07fe3d3fa
kuzminkdk <kuzminkdk@gmail.com>
2026-09-17 00:19:56 +0300

    Optimize Open Graph image

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -61,6 +61,7 @@ export async function generateMetadata(): Promise<Metadata> {
 			],
 		},
 		twitter: {
+			card: "summary_large_image",
 			title: "Daniel Cosmo's personal websit",
 			description:
 				"Daniel Cosmo's (kuzminklk) personal website about development, photography, sport…",
